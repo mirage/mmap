e27280046022fe31abd7de0e5b1907441e42c217
Jeremie Dimino <jeremie@dimino.org>
2019-02-26 10:03:40 +0000

    Initial import
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

---
+++ src/mmap.ml
@@ -0,0 +1,12 @@
+external map_internal :
+     Unix.file_descr
+  -> ('a, 'b) Bigarray.kind
+  -> 'c Bigarray.layout
+  -> bool
+  -> int array
+  -> int64
+  -> ('a, 'b, 'c) Bigarray.Genarray.t
+  = "caml_unix_map_file_bytecode" "caml_unix_map_file"
+
+let map_file fd ?(pos = 0L) kind layout shared dims =
+  map_internal fd kind layout shared dims pos
---
+++ src/mmap.mli
@@ -0,0 +1,9 @@
+(** Same as [Unix.map_file] in OCaml >= 4.06.0 *)
+val map_file :
+     Unix.file_descr
+  -> ?pos:int64
+  -> ('a, 'b) Bigarray.kind
+  -> 'c Bigarray.layout
+  -> bool
+  -> int array
+  -> ('a, 'b, 'c) Bigarray.Genarray.t
