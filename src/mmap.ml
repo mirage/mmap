68ffa35811de177deb48f67b8a11246f447e9bb3
Jeremie Dimino <jeremie@dimino.org>
2019-03-06 10:06:52 +0000

    Move map_file to a V1 submodule
    This will make it easier to change the API in the future if we want to
    
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- src/mmap.ml
+++ src/mmap.ml
@@ -1,12 +1,14 @@
-external map_internal :
-     Unix.file_descr
-  -> ('a, 'b) Bigarray.kind
-  -> 'c Bigarray.layout
-  -> bool
-  -> int array
-  -> int64
-  -> ('a, 'b, 'c) Bigarray.Genarray.t
-  = "caml_unix_map_file_bytecode" "caml_unix_map_file"
+module V1 = struct
+  external map_internal :
+       Unix.file_descr
+    -> ('a, 'b) Bigarray.kind
+    -> 'c Bigarray.layout
+    -> bool
+    -> int array
+    -> int64
+    -> ('a, 'b, 'c) Bigarray.Genarray.t
+    = "caml_unix_map_file_bytecode" "caml_unix_map_file"
 
-let map_file fd ?(pos = 0L) kind layout shared dims =
-  map_internal fd kind layout shared dims pos
+  let map_file fd ?(pos = 0L) kind layout shared dims =
+    map_internal fd kind layout shared dims pos
+end
--- src/mmap.mli
+++ src/mmap.mli
@@ -1,9 +1,11 @@
-(** Same as [Unix.map_file] in OCaml >= 4.06.0 *)
-val map_file :
-     Unix.file_descr
-  -> ?pos:int64
-  -> ('a, 'b) Bigarray.kind
-  -> 'c Bigarray.layout
-  -> bool
-  -> int array
-  -> ('a, 'b, 'c) Bigarray.Genarray.t
+module V1 : sig
+  (** Same as [Unix.map_file] in OCaml >= 4.06.0 *)
+  val map_file :
+       Unix.file_descr
+    -> ?pos:int64
+    -> ('a, 'b) Bigarray.kind
+    -> 'c Bigarray.layout
+    -> bool
+    -> int array
+    -> ('a, 'b, 'c) Bigarray.Genarray.t
+end
