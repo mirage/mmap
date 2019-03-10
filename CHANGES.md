1063c36f42f7506ffddf48f76eec6ccd1f682c17
Anil Madhavapeddy <anil@recoil.org>
2019-03-10 21:41:39 +0000

    improve ocamldoc, update CHANGES

--- CHANGES.md
+++ CHANGES.md
@@ -1,4 +1,8 @@
-1.0 (06/03/2019)
-----------------
+## v1.0.1 (2019-06-10)
 
-Initial release.
+- Fix opam doc field so dune-release works (@avsm)
+- Improve ocamldoc (@avsm)
+
+## v1.0 2019-03-06
+
+- Initial release. (@diml)
--- src/mmap.mli
+++ src/mmap.mli
@@ -1,5 +1,6 @@
+(** Compat module for memory mapping files *)
+
 module V1 : sig
-  (** Same as [Unix.map_file] in OCaml >= 4.06.0 *)
   val map_file :
        Unix.file_descr
     -> ?pos:int64
@@ -8,4 +9,5 @@ module V1 : sig
     -> bool
     -> int array
     -> ('a, 'b, 'c) Bigarray.Genarray.t
+  (** [map_file] is the same as {!Unix.map_file} in OCaml >= 4.06.0 *)
 end
