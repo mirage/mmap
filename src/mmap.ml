00748a5e79a8dbdf5ea60e0e387efeac5892f7c4
Jérémie Dimino <jeremie@dimino.org>
2019-03-12 12:11:45 +0000

    Refactor & fix the build with older OCaml versions (#1)
    - simplify the C stubs
    - import the custom operations from the OCaml runtime
    
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- src/mmap.ml
+++ src/mmap.ml
@@ -1,4 +1,11 @@
 module V1 = struct
+  external init : _ Bigarray.Array1.t -> unit = "caml_mmap_init"
+
+  let init =
+    lazy
+      (let ba = Bigarray.Array1.create Bigarray.char Bigarray.c_layout 1 in
+       init ba)
+
   external map_internal :
        Unix.file_descr
     -> ('a, 'b) Bigarray.kind
@@ -7,8 +14,9 @@ module V1 = struct
     -> int array
     -> int64
     -> ('a, 'b, 'c) Bigarray.Genarray.t
-    = "caml_unix_map_file_bytecode" "caml_unix_map_file"
+    = "caml_mmap_map_file_bytecode" "caml_mmap_map_file"
 
   let map_file fd ?(pos = 0L) kind layout shared dims =
+    Lazy.force init;
     map_internal fd kind layout shared dims pos
 end
